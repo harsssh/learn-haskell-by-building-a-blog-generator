ab5d00a35425ae9cb0893b4d157f2b18039495cf
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-11-23 01:33:48 +0900

    feat: add HTML escaping for title and text content

--- Html.hs
+++ Html.hs
@@ -24,13 +24,25 @@ html_ title content =
   Html
     ( el
         "html"
-        ( el "head" (el "title" title)
+        ( el "head" (el "title" (escape title))
             <> el "body" (getStructureString content)
         )
     )
 
 p_ :: String -> Structure
-p_ = Structure . el "p"
+p_ = Structure . el "p" . escape
 
 h1_ :: String -> Structure
-h1_ = Structure . el "h1"
+h1_ = Structure . el "h1" . escape
+
+escape :: String -> String
+escape =
+  let escapeChar c =
+        case c of
+          '<' -> "&lt;"
+          '>' -> "&gt;"
+          '&' -> "&amp;"
+          '"' -> "&quot;"
+          '\'' -> "&#39;"
+          _ -> [c]
+   in concatMap escapeChar
