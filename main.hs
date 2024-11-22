50c7fbef38d01579c2a686884895503890430b03
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-11-22 23:48:18 +0900

    refactor: modularize HTML generation functions

--- main.hs
+++ main.hs
@@ -1,5 +1,10 @@
-wrapHtml content = "<html><body>" <> content <> "</body></html>"
+html_ content = "<html>" <> content <> "</html>"
+head_ content = "<head>" <> content <> "</head>"
+title_ content = "<title>" <> content <> "</title>"
+body_ content = "<body>" <> content <> "</body>"
 
-myhtml = wrapHtml "Hello, world!"
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
+myhtml = makeHtml "My page title" "My page content"
 
 main = putStrLn myhtml
