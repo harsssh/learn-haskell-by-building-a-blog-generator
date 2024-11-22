be80712855c121e8f10e2fa29d81e0ad5dec69a4
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-11-23 00:09:03 +0900

    feat: add paragraph and header functions to HTML generator

--- main.hs
+++ main.hs
@@ -14,11 +14,20 @@ title_ = el "title"
 body_ :: String -> String
 body_ = el "body"
 
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
 myhtml :: String
-myhtml = makeHtml "My page title" "My page content"
+myhtml =
+  makeHtml
+    "My page title"
+    (h1_ "Hello" <> p_ "Haskell")
 
 main :: IO ()
 main = putStrLn myhtml
