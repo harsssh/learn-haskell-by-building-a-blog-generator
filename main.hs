8dc91690e0b13c04f9b882b2e73a6d9df3eeb94c
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-11-23 00:02:25 +0900

    refactor: simplify HTML element creation with a generic function

--- main.hs
+++ main.hs
@@ -1,8 +1,20 @@
-html_ content = "<html>" <> content <> "</html>"
-head_ content = "<head>" <> content <> "</head>"
-title_ content = "<title>" <> content <> "</title>"
-body_ content = "<body>" <> content <> "</body>"
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
+html_ :: String -> String
+html_ = el "html"
+
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+body_ :: String -> String
+body_ = el "body"
+
+makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
 myhtml = makeHtml "My page title" "My page content"
