9de6ae8b693863e075117145ad1d55f3fe2ef703
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-11-23 00:03:49 +0900

    型を明示

--- main.hs
+++ main.hs
@@ -17,6 +17,8 @@ body_ = el "body"
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
+myhtml :: String
 myhtml = makeHtml "My page title" "My page content"
 
+main :: IO ()
 main = putStrLn myhtml
