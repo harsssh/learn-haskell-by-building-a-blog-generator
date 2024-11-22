02be48ef9a12f6e0122d8e00301cc638c04038bb
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-11-23 02:05:05 +0900

    feat: add list and code elements to Html module

--- Html.hs
+++ Html.hs
@@ -1,3 +1,3 @@
-module Html (Html, Title, Structure, html_, p_, h1_, append_, render) where
+module Html (Html, Title, Structure, html_, p_, h1_, ul_, ol_, code_, append_, render) where
 
 import Html.Internal
