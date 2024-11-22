a32be419c901c4e7ede908acf549339c0f9eddc8
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-11-23 01:14:25 +0900

    refactor: restructure HTML generation using newtypes and functions

--- main.hs
+++ main.hs
@@ -1,33 +1,42 @@
+newtype Html = Html String
+
+newtype Structure = Structure String
+
+type Title = String
+
+getStructureString :: Structure -> String
+getStructureString (Structure str) = str
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure s) (Structure t) = Structure (s <> t)
+
+render :: Html -> String
+render (Html s) = s
+
 el :: String -> String -> String
 el tag content =
   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
-html_ :: String -> String
-html_ = el "html"
+html_ :: Title -> Structure -> Html
+html_ title content =
+  Html
+    ( el
+        "html"
+        (el "head" (el "title" title))
+        <> el "body" (getStructureString content)
+    )
 
-head_ :: String -> String
-head_ = el "head"
+p_ :: String -> Structure
+p_ = Structure . el "p"
 
-title_ :: String -> String
-title_ = el "title"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
-body_ :: String -> String
-body_ = el "body"
-
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
-
-makeHtml :: String -> String -> String
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
-
-myhtml :: String
+myhtml :: Html
 myhtml =
-  makeHtml
+  html_
     "My page title"
-    (h1_ "Hello" <> p_ "Haskell")
+    (append_ (h1_ "Heading") (p_ "Paragram #1"))
 
 main :: IO ()
-main = putStrLn myhtml
+main = putStrLn (render myhtml)
