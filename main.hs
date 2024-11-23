c8f7fd85177658230e095524755321ea04d1e667
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-11-23 17:32:42 +0900

    feat: add file processing and parsing functionality to main.hs

--- main.hs
+++ main.hs
@@ -1,10 +1,41 @@
+{-# LANGUAGE LambdaCase #-}
+
+import Control.Monad qualified
+import Convert (convert)
 import Html
+import Markup qualified
+import System.Directory (doesFileExist)
+import System.Environment (getArgs)
 
 main :: IO ()
-main = putStrLn (render myhtml)
+main =
+  getArgs >>= \case
+    [] ->
+      getContents >>= \content ->
+        putStrLn $ process "Empty title" content
+    [infile, outfile] ->
+      doesFileExist outfile >>= \exists ->
+        let writeResult = readFile infile >>= \content -> writeFile outfile $ process infile content
+         in if exists
+              then whenIO confirm writeResult
+              else writeResult
+    _ ->
+      putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
 
-myhtml :: Html
-myhtml =
-  html_
-    "My page title"
-    (h1_ "Heading" <> p_ "Paragram #1")
+process :: Html.Title -> String -> String
+process title = render . convert title . Markup.parse
+
+whenIO :: IO Bool -> IO () -> IO ()
+whenIO cond action =
+  cond >>= \result -> Control.Monad.when result action
+
+confirm :: IO Bool
+confirm =
+  putStrLn "Are you sure? (y/n)"
+    *> getLine
+    >>= \case
+      "y" -> pure True
+      "n" -> pure False
+      _ ->
+        putStrLn "Invalid response. use y or n"
+          *> confirm
